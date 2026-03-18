bb804b84b945624c1d706bc1259f12e112695bd8
Nadia Salem <nsalemprofessional@gmail.com>
2026-03-17 17:43:28 -0700

    Test styles file setup (confirmed working, UI intentionally ugly for visibility)

---
+++ app/(tabs)/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  padding: 20,
+  backgroundColor: 'black',
+  borderWidth: 10,
+  borderColor: 'yellow',
+}
+});
+
+export default styles;
